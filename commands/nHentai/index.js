e12760fa23a3faf236cfd31f143559dfeefa918c
cCurmudgeon <mpaarthurnax@gmail.com>
2021-03-27 00:10:42 +0530

    nHentai #2

--- commands/nHentai/index.js
+++ commands/nHentai/index.js
@@ -1,19 +1,36 @@
-const search = require('./src/search');
+const { response } = require("express");
+const { API } = require("nhentai-api");
+const api = new API();
 
 module.exports = {
-name: 'nhentai',
-execute(message, args){
+  name: "nhentai",
+  execute(message, args) {
+    const search = async (searchArg) => {
+      await api.getBook(searchArg).then((book) => {
+        console.log(book);
 
-    async function s() {
-        let response;
-        response = await search.search(args[0]);
+        const data = {
+          title: book.title.pretty,
+          id: book.id,
+          cover: book.cover[0],
+          upload: book.uploaded,
+        };
+        const embed = {
+          title: data.title,
+          thumbnail: {
+            url: data.cover,
+          },
+          description: `${data.id} at ${data.upload}`,
+        };
+        message.channel.send({embed: embed});
+      });
+    };
 
-        message.channel.send({
-            embed: {
-                ...response
-            }
-        });
-        
-    }s();
+    async function a(){
+      if (args[0] === "book" || args[0] === "b") {
+        await search(args[1]);
 
-}};
+      }
+    }a();
+  },
+};
