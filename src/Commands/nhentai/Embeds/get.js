dfc65a0dcf8182e3f6ae25a9c44849f85c257bcc
cCurmudgeon <mpaarthurnax@gmail.com>
2021-04-04 19:21:15 +0530

    Completed nhentai

---
+++ src/Commands/nhentai/Embeds/get.js
@@ -0,0 +1,65 @@
+function getBed(data, color) {
+  let categori = [];
+  let tagg = [];
+
+  data.category.forEach((cate) => {
+    categori.push(cate.hyptxt);
+  });
+  data.tags.forEach((tag) => {
+    tagg.push(tag.hyptxt);
+  });
+  if (tagg.toString().length >= 1000) {
+    while (tags.toString().length >= 1000) {
+      tagg.pop();
+    }
+  }
+  const embed = {
+    color: color,
+    title: data.details.title.pretty,
+    author: {
+      name: "nhentai",
+      icon_url: "https://i.imgur.com/hdUkQgO.png",
+    },
+    url: data.details.title.link,
+    thumbnail: {
+      url: data.images[0],
+    },
+    fields: [
+      {
+        name: "ID",
+        value: data.details.id,
+        inline: true,
+      },
+      {
+        name: "Language",
+        value: data.language[0].hyptxt,
+        inline: true,
+      },
+      {
+        name: "Category",
+        value: categori.join(", "),
+        inline: true,
+      },
+      {
+        name: "Tags",
+        value: tagg.join(", "),
+        inline: false,
+      },
+      {
+        name: "English Title",
+        value: data.details.title.english,
+        inline: false,
+      },
+      {
+        name: "Native Title",
+        value: data.details.title.japanese,
+        inline: false,
+      },
+    ],
+    footer: {
+      text: "Uploaded on: " + data.details.upload_date_pretty,
+    },
+  };
+  return embed;
+}
+module.exports = { getBed };
---
+++ src/Commands/nhentai/index.js
@@ -0,0 +1,29 @@
+const battleship = require("kongou");
+const kongou = new battleship();
+const { nhentai } = require("../../colors.json");
+const { getBed } = require("./Embeds/get");
+
+module.exports = {
+  name: "nhentai",
+  description: "nhentai API search.",
+  permission: ["SEND_MESSAGES"],
+  args: true,
+  usage: "<search/get> <keyword/id>",
+  category: "Features",
+
+  async execute(message, args, prefix) {
+    const arg = message.content.slice(
+      prefix.length + 1 + this.name.length + 1 + args[0].length
+    );
+    if (args[0] === "get") {
+      const data = getBed(await kongou.get(parseInt(arg)), nhentai);
+      message.channel.send({embed: data});
+    }
+    if(args[0] === "search"){
+        const res = await kongou.search(arg, 1, 1);
+        const data = getBed(res, nhentai);
+        message.channel.send({embed: data});
+    }
+    else return message.reply(this.name + ' only has ' + this.usage + '.');
+  },
+};
