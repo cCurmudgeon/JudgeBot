011b310fa9aaf63682caef90fc987573d418edb1
Rachala Ovin <resistlyfe@gmail.com>
2021-06-16 16:10:34 +0530

    initial

--- src/Commands/Anilist/Embeds/define.js
+++
@@ -1,8 +0,0 @@
-const { aniInfoBed } = require("./anime");
-const { manInfoBed } = require("./manga");
-const { usrInfoBed } = require("./user");
-const { charInfoBed } = require("./character");
-
-module.exports = {
-    aniInfoBed, manInfoBed, usrInfoBed, charInfoBed
-};
--- src/Commands/MangaDex/index.js
+++
@@ -1,8 +0,0 @@
-const dex = require("mangadex-full-api");
-
-const uinfo = {
-  uname: "_curmudgeon",
-  pword: "rachala2005",
-};
-
-dex.Manga.prototype;
